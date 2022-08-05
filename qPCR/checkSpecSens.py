c1da4f070058fa6b216a14a451ab80d569611827
GimenezEGT <gimenezenrico@yahoo.com.br>
2022-08-05 16:11:16 -0300

    example_data moved

--- qPCR/checkSpecSens.py
+++ qPCR/checkSpecSens.py
@@ -5,3 +5,11 @@ from Bio.Seq import Seq
 import pandas as pd
 import numpy as np
 
+
+def checkSpecSens(file):
+    df = pd.read_csv(file, delimiter="\t", header=0)
+    target_taxid = df[['taxid']][0]
+    print(target_taxid)
+
+
+checkSpecSens('./example_data.tsv')
