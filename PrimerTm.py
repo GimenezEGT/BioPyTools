cc2df7b1bd1526c68266b5d997d36bdc11b8c273
GimenezEGT <gimenezenrico@yahoo.com.br>
2022-08-04 23:31:40 -0300

    calculo de diferenca de tm e class. iniciados

--- PrimerTm.py
+++ PrimerTm.py
@@ -8,11 +8,11 @@ import numpy as np
 
 
 def getTm(archive):
-    with open(archive, "w") as file:
+    with open(archive, "r") as file:
         # Open file and determine dataframe
         df = pd.read_csv(file, delimiter="\t", header=0)
         print(df.columns)
-        qseq = df[['qseq']]
+        # qseq = df[['qseq']]
         print('Sequences achieved!')
         Tm_values = list()
 
@@ -24,6 +24,21 @@ def getTm(archive):
                              Mg=1.5, Tris=10, dnac1=500, dNTPs=0.8, Na=65))
         df['tm'] = np.resize(Tm_values, len(df))
         print(df)
-        file.write(df)
+
+        # Calculating diference between annealings and primer Tm
+        tm_diference = []
+        tm_primer = df['tm'][0]
+        
+        print(f'Your primer has a melting temperature of {tm_primer}ºC')
+        for index, row in df[['tm']].iterrows():
+            tm_annealing = row['tm']
+            diference = tm_primer - tm_annealing
+            tm_diference.append(diference)
+            if diference > 5 or diference < (-5):
+
+        # Overwrite file with Tm's 
+        # with open(archive, 'w') as writable:
+        #     df.to_csv(writable,sep="\t", encoding='utf-8')
+        
 
 getTm('./example_data.tsv')
