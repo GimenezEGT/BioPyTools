a52a8d97c1331e4b2752a065df3eedacd0ac2804
GimenezEGT <gimenezenrico@yahoo.com.br>
2022-08-05 17:44:17 -0300

    checkSpec populated!

--- qPCR/checkSpecSens.py
+++ qPCR/checkSpecSens.py
@@ -8,8 +8,18 @@ import numpy as np
 
 def checkSpecSens(file):
     df = pd.read_csv(file, delimiter="\t", header=0)
-    target_taxid = df[['taxid']][0]
-    print(target_taxid)
+    target_taxid = df['staxids'][0]
 
+    for index, row in df[['staxids']].iterrows():
+        if row['staxids'] != target_taxid and df['same_tm'][index] == True:
+            pass # contar linhas nesta condição, dizer quais as espécies que pega
+        elif row['staxids'] != target_taxid and df['same_tm'][index] == False:
+            pass # contar linhas nesta condição
+        elif row['staxids'] == target_taxid and df['same_tm'][index] == True:
+            pass # contar linhas nesta condição, dizer quais as espécies que pega
+        elif row['staxids'] == target_taxid and df['same_tm'][index] == False:
+            pass # contar linhas nesta condição, dizer quais as espécies que pega
+        else:
+            print('FATAL ERROR: ERR01 - NO CONDITIONS AVAILABLE')
 
 checkSpecSens('./example_data.tsv')
