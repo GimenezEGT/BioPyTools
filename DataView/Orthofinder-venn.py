69cd3cdd41dfad5c7416eade8587b9dd8d4d991a
GimenezEGT <gimenezenrico@yahoo.com.br>
2022-08-15 17:33:09 -0300

    DataView added

---
+++ DataView/Orthofinder-venn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.8
+
+#import libraries
+from ast import increment_lineno
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt
+
+
+# Creating graphs
+# grafico = venn2(subsets=(30, 10, 5), set_labels=("Group A", "Group B"))
+
+# print(grafico)
+
+# plt.title("")
+# plt.show()
