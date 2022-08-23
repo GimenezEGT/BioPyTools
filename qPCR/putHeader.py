466f039e409aba0839edb7c2dd9724de33dd1d28
GimenezEGT <gimenezenrico@yahoo.com.br>
2022-08-23 14:22:30 -0300

    alteração no puteHeader

--- qPCR/putHeader.py
+++ qPCR/putHeader.py
@@ -9,12 +9,16 @@ def putHeader():
     directory = './'
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        file = open(f, 'r')
+        file = open(f, 'r', encoding='iso8859-1')
         line = 0
         header = ('qseqid	sscinames	qcovs	pident	evalue	staxids	qseq')
         lines = file.readlines()
         file.close()
         lines.insert(line, header + "\n")
-        file = open(f, "w")
+        file = open(f, "w", encoding='iso8859-1')
         file.writelines(lines)
         file.close()
+
+
+if __name__ == "__main__":
+    putHeader()
